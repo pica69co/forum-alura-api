1a83c761fbb259c23deb1f9820b9d0d5b3cac4d2
pica69co <oscarwarrieta@gmail.com>
2024-06-30 19:09:42 -0500

    Initial commit

---
+++ forum-alura-api/src/main/java/com/warrieta/forum/forum/alura/api/ForumAluraApiApplication.java
@@ -0,0 +1,13 @@
+package com.warrieta.forum.forum.alura.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumAluraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumAluraApiApplication.class, args);
+	}
+
+}
---
+++ forum-alura-api/src/test/java/com/warrieta/forum/forum/alura/api/ForumAluraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warrieta.forum.forum.alura.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumAluraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
